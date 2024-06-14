a7a093e77f440e337354227e5f35d59f35ed0e00
Jorge Luis Sanchez Mendoza <57418730+jorgeula300@users.noreply.github.com>
2024-06-14 10:35:47 -0500

    img corregidas

--- src/components/HomePages/HotelCard.jsx
+++ src/components/HomePages/HotelCard.jsx
@@ -9,17 +9,17 @@ const HotelCard = ({ hotel }) => {
         navigate(`/hotels/${hotel.id}`)
     }
 
-    const data = hotel?.reviews.map(element => {
-        let sum = 0
-        sum += element.rating
-        return sum / hotel.reviews.length
-    })
+    const suma = hotel?.reviews.map(item => item.rating).reduce((acc, curr) => acc + curr, 0);
+
+
+    const raiting = suma / hotel?.reviews.length
+
 
 
     return (
         <article className=" rounded-xl w-[211px] border shadow">
             <header className=" rounded-t-xl overflow-hidden ">
-                <img className="max-h-[11em] w-full max-w-[211px]" src={hotel.images[0].url} alt={hotel.name} />
+                <img className="h-[11em] w-full max-w-[211px]" src={hotel.images[0].url} alt={hotel.name} />
             </header>
             <section className="flex flex-col justify-center mt-4 mx-3 mb-3 space-y-2">
                 <div className=" h-[50px]">
@@ -27,7 +27,7 @@ const HotelCard = ({ hotel }) => {
                 </div>
 
                 <div className=" h-[60px] flex flex-col justify-center ">
-                    <p> Rating: {data}</p>
+                    <p> Rating: {raiting}</p>
                     <span className=" text-xs font-medium ">{hotel.city.name}, {hotel.city.country}</span>
                     <span className=" text-xs font-semibold">$ {hotel.price}</span>
 
--- src/pages/HotelsIdPage.jsx
+++ src/pages/HotelsIdPage.jsx
@@ -19,11 +19,9 @@ const HotelsIdPage = () => {
     getHotels(url)
   }, [id])
 
-  const data = hotels?.reviews.map(element => {
-    let sum = 0
-    sum += element.rating
-    return sum / hotels.reviews.length
-  })
+  const suma = hotels?.reviews.map(item => item.rating).reduce((acc, curr) => acc + curr, 0);
+
+  const raiting = suma / hotels?.reviews.length
 
 
 
@@ -31,7 +29,7 @@ const HotelsIdPage = () => {
   return (
     <div className=" w-full max-w-[1200px] mx-auto px-4 py-20">
       <h2 className=" text-center font-bold text-3xl">{hotels?.result.name}</h2>
-      <h3 className=" text-center font-medium text-xl">RATING- {data}</h3>
+      <h3 className=" text-center font-medium text-xl">RATING- {raiting}</h3>
 
       <div className=" flex  md:flex-row flex-col justify-center items-center space-x-4">
         <SlaiderImgs hotels={hotels} />
